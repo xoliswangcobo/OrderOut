8ed9a14028be07736b9da9754c804f516b7c4698
Xoliswa Ngcobo <xoliswangcobo@icloud.com>
2020-08-29 15:53:35 +0200

    Created new project

---
+++ OrderOut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrderOut
+//
+//  Created by Xoliswa on 2020/08/29.
+//  Copyright © 2020 Xoliswa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
